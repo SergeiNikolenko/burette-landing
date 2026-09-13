10870e007c59bd0381353c7694a7cf83745b8eaa
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-09-13 22:15:15 +0300

    Add 1HTB protein complex to the presentation

--- components/landing/workspace-scenes.js
+++ components/landing/workspace-scenes.js
@@ -1,4 +1,5 @@
 export const workspaceScenes = [
   { label: "Structures", path: "/BuretteDemo/proteins/7RPZ.pdb", direction: 1 },
   { label: "Crystals", path: "/BuretteDemo/crystals/caffeine.cif", direction: -1 },
+  { label: "Protein complex", path: "/BuretteDemo/proteins/1HTB.pdb", direction: 1 },
 ];
