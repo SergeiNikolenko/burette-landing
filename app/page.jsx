34556ca99e8a8df8af9ed6a687db9098ffda723d
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-09-23 14:25:31 +0300

    Polish mobile landing and feature layouts

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import "./landing.css";
 import "./editorial.css";
 import "./live-demo.css";
 import "./home-stories.css";
+import "./mobile.css";
 import { SITE_URL } from "./site-url.js";
 import Analytics from "@/components/landing/analytics";
 import SiteNav from "@/components/landing/site-nav";
